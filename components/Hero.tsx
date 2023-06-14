b4027e708d6c37524bbd4d91212986d742988027
sandip-cagtu <sandipthapa@cagtu.com>
2023-06-14 22:23:37 +0545

    feat(Hero Section): Added hero image section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Button from "./Button";
+import Image from "next/image";
 
 const Hero = () => {
     const handleScroll = () => {};
@@ -19,6 +20,18 @@ const Hero = () => {
                     containerStyles="bg-primary-blue text-white rounded-full mt-10"
                     handleClick={handleScroll}
                 />
+
+                <div className="hero__image-container">
+                    <div className="hero__image">
+                        <Image
+                            src={"/hero.png"}
+                            alt="hero image"
+                            fill
+                            className="object-contain"
+                        />
+                        <div className="hero__image-overlay"></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
