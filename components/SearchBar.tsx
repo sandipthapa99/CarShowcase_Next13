01630eb66fabd64dba6dc2931c6b2c23e0846f94
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-16 22:37:44 +0545

    feat(Components): Added search and filter components

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchBar = () => {
+    const handleSubmit = () => {};
+
+    const [manufacturer, setManufacturer] = useState("");
+
+    return (
+        <form className="searchbar" onSubmit={handleSubmit}>
+            <div className="searchbar__item">
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    );
+};
+
+export default SearchBar;
