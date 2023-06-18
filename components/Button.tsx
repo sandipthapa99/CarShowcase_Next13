674aa3711465b10842988b762448b779d27cd479
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-18 22:53:06 +0545

    feat(Button Component): Updated Button to accept more props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ButtonProps } from "@/types";
+import Image from "next/image";
 import React from "react";
 
 const Button = ({
@@ -7,6 +8,8 @@ const Button = ({
     containerStyles,
     handleClick,
     btnType,
+    textStyles,
+    rightIcon,
 }: ButtonProps) => {
     return (
         <button
@@ -15,7 +18,17 @@ const Button = ({
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
         >
-            <span className={`flex-1`}>{title}</span>
+            <span className={`flex-1 ${textStyles}`}>{title}</span>
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image
+                        src={rightIcon}
+                        alt="right icon"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+            )}
         </button>
     );
 };
