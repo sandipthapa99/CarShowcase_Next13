cb5bfb7f929a8b8ac7014e54ff9976b04906c009
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-18 22:53:54 +0545

    feat(Index): Added section for cars listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+    const allCars = await fetchCars();
+
+    const isDataEmpty =
+        !Array.isArray(allCars) || allCars.length < 1 || !allCars;
     return (
         <main className="overflow-hidden">
             <Hero />
@@ -18,6 +23,22 @@ export default function Home() {
                         <CustomFilter />
                     </div>
                 </div>
+
+                {!isDataEmpty ? (
+                    <section>
+                        <div className="home__cars-wrapper">
+                            {allCars?.map((car, index) => (
+                                <CarCard car={car} key={index} />
+                            ))}
+                        </div>
+                    </section>
+                ) : (
+                    <div className="home__error-container">
+                        <h2 className="text-black text-xl font-bold">
+                            oops, No result
+                        </h2>
+                    </div>
+                )}
             </div>
         </main>
     );
