eff2a5ca97595414466d439c69997b2f2b650f5d
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-18 22:54:41 +0545

    feat(Component): Added component to display car details using a card

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetails from "./CardDetails";
 
 export {
     Hero,
@@ -14,4 +16,6 @@ export {
     CustomFilter,
     SearchBar,
     SearchManufacturer,
+    CarCard,
+    CardDetails,
 };
