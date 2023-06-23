4f05c79a839581fa9ba0a253e0c6337441e934e2
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-23 22:30:39 +0545

    (Component): Added search component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,20 +1,55 @@
 "use client";
 import React, { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image
+            src={"/magnifying-glass.svg"}
+            alt="magnifying glass"
+            height={40}
+            width={40}
+            className="object-contain"
+        />
+    </button>
+);
 
 const SearchBar = () => {
-    const handleSubmit = () => {};
+    const router = useRouter();
 
     const [manufacturer, setManufacturer] = useState("");
+    const [model, setModel] = useState("");
 
     return (
-        <form className="searchbar" onSubmit={handleSubmit}>
+        <form className="searchbar">
             <div className="searchbar__item">
                 <SearchManufacturer
                     manufacturer={manufacturer}
                     setManufacturer={setManufacturer}
                 />
+                <SearchButton otherClasses="sm:hidden" />
             </div>
+            <div className="searchbar__item">
+                <Image
+                    src={"/model-icon.png"}
+                    height={25}
+                    width={25}
+                    className="absolute w-[20px] h-[20px] ml-4"
+                    alt="car model"
+                />
+                <input
+                    type="text"
+                    name="model"
+                    value={model}
+                    onChange={(e) => setModel(e.target.value)}
+                    placeholder="Tiguan"
+                    className="searchbar__input"
+                />
+                <SearchButton otherClasses="sm:hidden" />
+            </div>
+            <SearchButton otherClasses="max-sm:hidden" />
         </form>
     );
 };
