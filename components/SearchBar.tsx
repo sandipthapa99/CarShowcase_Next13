6f07c24adfb96e9ded3dd3db94989db460e91e65
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-23 22:32:26 +0545

    feat(Search query): Added functionality to update search query using URLSearchParams

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,11 +19,42 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
     const router = useRouter();
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === "" && model === "") {
+            return alert("Please fill in the search bar");
+        }
+
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+    };
+
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        if (model) {
+            searchParams.set("model", model);
+        } else {
+            searchParams.delete("model");
+        }
+        if (manufacturer) {
+            searchParams.set("manufacturer", manufacturer);
+        } else {
+            searchParams.delete("manufacturer");
+        }
+
+        const newPathname = `${
+            window.location.pathname
+        }?${searchParams.toString()}`;
+
+        router.push(newPathname);
+    };
+
     const [manufacturer, setManufacturer] = useState("");
     const [model, setModel] = useState("");
 
     return (
-        <form className="searchbar">
+        <form className="searchbar" onSubmit={handleSubmit}>
             <div className="searchbar__item">
                 <SearchManufacturer
                     manufacturer={manufacturer}
