88fc77b80bf31fe58b899b1516f39e5d3c889af4
Sandip Thapa <sandipthapa386@gmail.com>
2023-06-25 17:44:18 +0545

    fix: Resolved type error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
 
-export default async function Home({ searchParams }) {
+export default async function Home(searchParams: any) {
     const allCars = await fetchCars({
         manufacturer: searchParams.manufacturer || "",
         year: searchParams.year || 2022,
